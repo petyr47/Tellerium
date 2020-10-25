97a987a5440d4a075de9a0ab356f4b313ab9b71c
petyr47 <peter@initsng.com>
2020-10-25 14:40:15 +0100

    modify injector

--- app/src/main/java/com/aneke/peter/tellerium/di/AppInjector.kt
+++ app/src/main/java/com/aneke/peter/tellerium/di/AppInjector.kt
@@ -2,7 +2,10 @@ package com.aneke.peter.tellerium.di
 
 import com.aneke.peter.tellerium.data.AppDatabase
 import com.aneke.peter.tellerium.data.PrefStore
+import com.aneke.peter.tellerium.login.LoginRepository
+import com.aneke.peter.tellerium.login.LoginViewModel
 import com.aneke.peter.tellerium.network.RetrofitClient
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 private val dataModule = module {
@@ -12,10 +15,12 @@ private val dataModule = module {
 }
 
 private val repositoryModule = module {
+    single { LoginRepository(get()) }
 
 }
 
 private val viewModels = module {
+    viewModel { LoginViewModel(get()) }
 
 }
 
