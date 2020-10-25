ca45ba885f5a06784bf974ae9591b2e1f5f80bab
petyr47 <peter@initsng.com>
2020-10-25 14:38:48 +0100

    setup session duration

--- app/src/main/java/com/aneke/peter/tellerium/data/PrefStore.kt
+++ app/src/main/java/com/aneke/peter/tellerium/data/PrefStore.kt
@@ -2,9 +2,12 @@ package com.aneke.peter.tellerium.data
 
 import android.content.Context
 import android.content.SharedPreferences
+import java.util.*
 
 const val SHARED_PREF = "shared_pref"
 const val IMAGE_BASE_URL = "image_base_url"
+const val LAST_LOGIN_TIME = "login_time"
+const val SESSION_LENGTH = 30 * 60 * 1000 //30 minutes
 
 class PrefStore (private val context : Context) {
 
@@ -18,4 +21,16 @@ class PrefStore (private val context : Context) {
     var imageBaseUrl : String
         set(value) = editor.putString(IMAGE_BASE_URL,value).apply()
         get() = sharedPref.getString(IMAGE_BASE_URL, "") ?: ""
+
+    var loginTime : Long
+        set(value) = editor.putLong(LAST_LOGIN_TIME, value).apply()
+        get() = sharedPref.getLong(LAST_LOGIN_TIME, 0)
+
+
+
+    fun isLoginValid() : Boolean {
+        val currentTime = Calendar.getInstance().timeInMillis
+        val difference = currentTime - loginTime
+        return SESSION_LENGTH > difference
+    }
 }
