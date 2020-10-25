bcce99637d6f79c87f09dd60c8879a0c8b2e2ea3
petyr47 <peter@initsng.com>
2020-10-25 14:36:43 +0100

    create DashboardActivity

---
+++ app/src/main/java/com/aneke/peter/tellerium/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.aneke.peter.tellerium.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aneke.peter.tellerium.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
