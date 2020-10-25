ba7fbcf6b532b465155160ac5765ba2a9b160298
petyr47 <peter@initsng.com>
2020-10-25 12:03:18 +0100

    modify injector

--- app/src/main/java/com/aneke/peter/tellerium/di/AppInjector.kt
+++ app/src/main/java/com/aneke/peter/tellerium/di/AppInjector.kt
@@ -1,5 +1,6 @@
 package com.aneke.peter.tellerium.di
 
+import com.aneke.peter.tellerium.data.AppDatabase
 import com.aneke.peter.tellerium.data.PrefStore
 import com.aneke.peter.tellerium.network.RetrofitClient
 import org.koin.dsl.module
@@ -7,7 +8,7 @@ import org.koin.dsl.module
 private val dataModule = module {
     single { RetrofitClient.makeApiService() }
     single { PrefStore(get()) }
-
+    single { AppDatabase.getInstance(get()) }
 }
 
 private val repositoryModule = module {
