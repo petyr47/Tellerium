1375bafad73292482150830ab808bd5ec8d08167
petyr47 <peter@initsng.com>
2020-10-25 14:40:48 +0100

    complete MainActivity

--- app/src/main/java/com/aneke/peter/tellerium/MainActivity.kt
+++ app/src/main/java/com/aneke/peter/tellerium/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.aneke.peter.tellerium
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.aneke.peter.tellerium.data.PrefStore
+import com.aneke.peter.tellerium.login.LoginActivity
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private val prefStore : PrefStore by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            resolveActivity()
+        }, 3500)
+
+
+    }
+
+
+    private fun resolveActivity() {
+        if (prefStore.isLoginValid()) {
+
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
