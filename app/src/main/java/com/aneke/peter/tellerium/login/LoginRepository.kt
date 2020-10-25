6a2d2687540f149f33c8ff928f4d76d404e0244f
petyr47 <peter@initsng.com>
2020-10-25 14:40:34 +0100

    create LoginRepository and LoginViewModel

---
+++ app/src/main/java/com/aneke/peter/tellerium/login/LoginRepository.kt
@@ -0,0 +1,7 @@
+package com.aneke.peter.tellerium.login
+
+import com.aneke.peter.tellerium.data.PrefStore
+
+class LoginRepository(private val prefStore: PrefStore) {
+
+}
---
+++ app/src/main/java/com/aneke/peter/tellerium/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.aneke.peter.tellerium.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel(private val repository: LoginRepository) : ViewModel() {
+
+}
