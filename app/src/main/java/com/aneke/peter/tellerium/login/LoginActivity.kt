96c3132075919076d90a9afb8c3863f66c8cfa3c
petyr47 <peter@initsng.com>
2020-10-26 13:52:54 +0100

    setup LoginActivity

--- app/src/main/java/com/aneke/peter/tellerium/login/LoginActivity.kt
+++ app/src/main/java/com/aneke/peter/tellerium/login/LoginActivity.kt
@@ -2,15 +2,46 @@ package com.aneke.peter.tellerium.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.aneke.peter.tellerium.R
+import com.aneke.peter.tellerium.databinding.ActivityLoginBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.log
 
 class LoginActivity : AppCompatActivity() {
 
     private val loginViewModel : LoginViewModel by viewModel()
+    private lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.lifecycleOwner = this
+        binding.viewModel = loginViewModel
+
+        loginViewModel.errorMessage.observe(this, Observer {
+            it?.let { message ->
+                showMessage(message)
+            }
+        })
+    }
+
+
+    private fun showMessage(message : String) {
+        val builder = AlertDialog.Builder(this).apply {
+            setTitle("Attention")
+            setMessage(message)
+            setPositiveButton("OK"){ dialog, _ ->
+                dialog.dismiss()
+            }
+        }
+        if (!isFinishing) {
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 }
--- app/src/main/java/com/aneke/peter/tellerium/login/LoginViewModel.kt
+++ app/src/main/java/com/aneke/peter/tellerium/login/LoginViewModel.kt
@@ -1,7 +1,37 @@
 package com.aneke.peter.tellerium.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.aneke.peter.tellerium.util.isEmailValid
 
 class LoginViewModel(private val repository: LoginRepository) : ViewModel() {
+
+
+    val userName = MutableLiveData<String>()
+    val errorMessage = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
+
+    private fun validateFields() : Boolean{
+        if (userName.value.isNullOrBlank()){
+            errorMessage.value = "Email cannot be blank"
+            return false
+        }
+        if (userName.value.isEmailValid()){
+            errorMessage.value = "Please enter a valid email"
+            return false
+        }
+        if (password.value.isNullOrBlank()){
+            errorMessage.value = "Password cannot be blank"
+            return false
+        }
+        return true
+    }
+
+    fun loginClicked() {
+        if (validateFields()) {
+
+        }
+    }
 
 }
