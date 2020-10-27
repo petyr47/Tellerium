e2c22767285e900a96d6b5530b4702fd852d9631
petyr47 <peter@initsng.com>
2020-10-27 01:54:14 +0100

    add Resource to handle responses

---
+++ app/src/main/java/com/aneke/peter/tellerium/data/Resource.kt
@@ -0,0 +1,26 @@
+package com.aneke.peter.tellerium.data
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+    companion object {
+        fun <T> success(data: T?, msg: String? ): Resource<T> {
+            return Resource(Status.SUCCESS, data, msg)
+        }
+
+        fun <T> error(msg: String?, data: T? = null): Resource<T> {
+            return Resource(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T? = null): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+    }
+}
+
+/**
+sets status for response from api calls
+ */
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
