06c02a4c28821733a83280695ec2a2fb0f349e37
petyr47 <peter@initsng.com>
2020-10-31 15:01:28 +0100

    show farmer list on db

--- app/src/main/java/com/aneke/peter/tellerium/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/aneke/peter/tellerium/dashboard/DashboardActivity.kt
@@ -2,11 +2,33 @@ package com.aneke.peter.tellerium.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import com.aneke.peter.tellerium.R
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DashboardActivity : AppCompatActivity() {
+
+    private val viewModel : DashboardViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val adapter = FarmerAdapter(this)
+        adapter.list = listOf()
+        adapter.onItemClicked = { farmer ->  
+            
+        }
+        farmer_list.adapter = adapter
+
+
+        viewModel.farmers.observe(this, Observer {
+            it?.let {list ->
+                adapter.list = list
+                adapter.notifyDataSetChanged()
+            }
+        })
+
     }
 }
