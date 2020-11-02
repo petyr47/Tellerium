820491be240d391e2f6563d18eb9dc80376f2b98
petyr47 <peter@initsng.com>
2020-11-02 01:14:59 +0100

    setup Update

--- app/src/main/java/com/aneke/peter/tellerium/update/UpdateFarmerActivity.kt
+++ app/src/main/java/com/aneke/peter/tellerium/update/UpdateFarmerActivity.kt
@@ -2,12 +2,19 @@ package com.aneke.peter.tellerium.update
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import coil.load
 import com.aneke.peter.tellerium.R
+import com.aneke.peter.tellerium.databinding.ActivityUpdateFarmerBinding
+import kotlinx.android.synthetic.main.activity_update_farmer.*
 import org.koin.android.ext.android.inject
 
 class UpdateFarmerActivity : AppCompatActivity() {
 
     private val viewModel : UpdateViewModel by inject()
+    private lateinit var binding : ActivityUpdateFarmerBinding
 
     private var id = 0
 
@@ -15,6 +22,47 @@ class UpdateFarmerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_farmer)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_update_farmer)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
         id = intent.getIntExtra("id", 0)
+        viewModel.fetchFarmer(id)
+
+        back_btn.setOnClickListener {
+            onBackPressed()
+        }
+        save.setOnClickListener {
+            showConfirmationDialog()
+        }
+
+
+        viewModel.farmer.observe(this, Observer { passport.load(viewModel.url)})
+    }
+
+
+    override fun onBackPressed() {
+        showConfirmationDialog()
+    }
+
+    private fun showConfirmationDialog() {
+        val builder = AlertDialog.Builder(this).apply {
+            setTitle("Save Changes")
+            setMessage("Do you want to save changes?")
+            setPositiveButton("YES"){ dialog, _ ->
+                viewModel.updateFarmer()
+                dialog.dismiss()
+                finish()
+            }
+            setNegativeButton("NO"){ dialog, _ ->
+                dialog.dismiss()
+                finish()
+            }
+        }
+
+        if (!isFinishing) {
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 }
--- app/src/main/java/com/aneke/peter/tellerium/update/UpdateRepository.kt
+++ app/src/main/java/com/aneke/peter/tellerium/update/UpdateRepository.kt
@@ -1,9 +1,32 @@
 package com.aneke.peter.tellerium.update
 
 import com.aneke.peter.tellerium.data.AppDatabase
+import com.aneke.peter.tellerium.data.PrefStore
+import com.aneke.peter.tellerium.network.models.Farmer
 
-class UpdateRepository(db : AppDatabase) {
+class UpdateRepository(db : AppDatabase, private val prefStore: PrefStore) {
 
     private val dao = db.farmerDao()
+
+    suspend fun fetchFarmerById(id : Int) : Farmer? {
+        try {
+           return dao.fetchFarmerWithId(id).single { it.id == id }
+        } catch (e : Exception){
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    fun imageUrl() = prefStore.imageBaseUrl
+
+    suspend fun updateFarmer(farmer: Farmer) : Boolean {
+        try {
+            dao.updateFarmer(farmer)
+            return true
+        } catch (e : Exception) {
+            e.printStackTrace()
+            return false
+        }
+    }
 
 }
